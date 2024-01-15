557eb8756a67c790b6501a433be079fe960b2ac2
pochi-pochi <takahiroa500@outlook.jp>
2024-01-15 17:30:53 +0900

    [add]基本機能の追加
    アプリの基本機能を追加しました。
    optionsにfirebaseの設定を追加しました。

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'screens/introduction_screen.dart';
 import 'screens/home_screen.dart';
+import 'config/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(MyApp());
 }
 
--- lib/screens/introduction_screen.dart
+++ lib/screens/introduction_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'services/authentication_service.dart';
+import '../services/authentication_service.dart';
 import 'home_screen.dart';
 
 class IntroductionScreen extends StatelessWidget {
